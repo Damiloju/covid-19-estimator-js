2f6e9c70d432bece359d22c73e4a9dd01eea0351
Oluwadamiloju <yusufdamiloju@gmail.com>
2020-04-07 18:46:43 +0100

    wip: finish challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,51 @@
-const covid19ImpactEstimator = (data) => data;
+const IMPACTLEVEL = {
+  NORMAL: 1,
+  SEVERE: 2
+};
+
+const calculateCurrentlyInfected = (reportedCases, impactLevel) => {
+  if (impactLevel === IMPACTLEVEL.NORMAL) {
+    return reportedCases * 10;
+  }
+
+  if (impactLevel === IMPACTLEVEL.SEVERE) {
+    return reportedCases * 50;
+  }
+
+  return reportedCases * 10;
+};
+
+const calculateinfectionsByRequestedTime = (currentlyInfected, factor) => {
+  const infectionQuotient = 2 ** factor;
+  return currentlyInfected * infectionQuotient;
+};
+
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = calculateCurrentlyInfected(input.reportedCases, 1);
+  severeImpact.currentlyInfected = calculateCurrentlyInfected(
+    input.reportedCases,
+    2
+  );
+
+  impact.infectionsByRequestedTime = calculateinfectionsByRequestedTime(
+    impact.currentlyInfected,
+    9
+  );
+
+  severeImpact.infectionsByRequestedTime = calculateinfectionsByRequestedTime(
+    severeImpact.currentlyInfected,
+    9
+  );
+
+  return {
+    data: input,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
